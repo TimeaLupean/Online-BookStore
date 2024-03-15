787a812ae20abe8457b831baa27745a4c442a035
timea <timea_lupean2003@yahoo.com>
2024-03-15 11:04:34 +0200

    Added files

---
+++ src/main/java/org/example/controllers/BookController.java
@@ -0,0 +1,41 @@
+package org.example.controllers;
+
+import org.example.main.*;
+import org.example.repositories.BooksRepository;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BookController {
+    private BooksRepository bookRepository;
+    private Scanner scanner = new Scanner(System.in);
+    private Clients currentClient;
+
+    public BookController(BooksRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void createBook(int book_id, String title, int publishing_year, Author author, int price, Category category) {
+        Books book = new Books(book_id, title, publishing_year, author, price, category);
+        bookRepository.save(book);
+    }
+
+    public Books findBookById(int bookID) {
+        return bookRepository.findById(bookID);
+    }
+
+    public List<Books> viewAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public void updateBook(int book_id, String title, int publishing_year, Author author, int price, Category category) {
+        Books updatedbook = new Books(book_id, title, publishing_year, author, price, category);
+        bookRepository.update(updatedbook);
+    }
+
+    public void deleteBook(int bookId) {
+        bookRepository.delete(bookId);
+    }
+
+
+}
